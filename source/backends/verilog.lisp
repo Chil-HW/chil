2c609a43a364603b302be0c9cb4fc423291a36bd
Karl Hallsby <karl@hallsby.com>
2025-02-26 00:16:05 -0600

    REBASE & SPLIT ME: Switch Verilog codegen to backend class generator
    This uses something _similar_ to the Visitor pattern, but because
    methods are just specialized functions, rather than being attached to
    objects, we can actually skip the double-indirection of the visitor
    pattern and just write something that looks natural. The runtime checks
    will do the proper dynamic dispatch for us.
    
    Update Verilog unit test to use new backend stuff

--- source/backends/verilog.lisp
+++ source/backends/verilog.lisp
@@ -10,39 +10,45 @@
 
 (in-package :chil/backends/verilog)
 
-(defun chil-sym->verilog-sym (str)
-  "Replace characters in STR that are invalid for Verilog symbols."
-  ;; TODO: Need a better way to map characters to replace to known-good
-  ;; characters for that particular HDL.
-  (let ((chars-to-replace (cl-ppcre:create-scanner "-")))
-    (cl-ppcre:regex-replace-all chars-to-replace str "_")))
+
+;;;
+;;; Verilog Backend Structure
+;;;
 
-(defun generate-and (form)
-  (trivia:match form
-    ;; FIXME: Matching should require the symbol 'and in the list!
-    ;; FIXME: and match should allow for unlimited arguments, like Lisps do.
-    ((list _ lhs rhs)
-      (format 'nil "~{~a~^ & ~}" (list lhs rhs)))))
+(defclass verilog-module (chil/backends:backend-hdl)
+  ((name
+    :reader name
+    :initarg :name
+    :type string
+    :documentation "Verilog module's name.")
+   ;; FIXME: Do we actually need to propagate parameter information? The Chil
+   ;; level of the language should handle elaborating and resolving all
+   ;; parameters, shouldn't it? But what about future interoperability?
+   (parameters
+    :reader parameters
+    :initarg :parameters
+    :initform '()
+    ;; :type alist-of-string-vals
+    :documentation "Parameters that this module should have.")
+   (inputs
+    :reader inputs
+    :initarg :inputs
+    ;; :type list-of-verilog-net
+    :documentation "Input wires to this Verilog module.")
+   (outputs
+    :reader outputs
+    :initarg :outputs
+    ;; :type list-of-verilog-net
+    :documentation "Output wires from this Verilog module.")
+   (body
+    :reader body
+    :initarg :body
+    :type verilog-net
+    :documentation "The body of this Verilog module."))
+  (:documentation "Verilog-specific backend module."))
 
-(defun generate-assign (form)
-  (trivia:match form
-    ;; FIXME: Matching should require the symbol 'assign in the list!
-    ((list _ res rhs)
-     ;; (format 'nil "assign ~a = ~a" res (generate-body rhs))
-     (format 'nil "assign ~a = ~a;" res (generate-and rhs)))
-    (nil
-     (format 'nil "body;"))))
-
-;; TODO: We want to create something akin to a dispatch table
-(defmethod generate-body (body-list)
-  (if (not (eql body-list 'nil))
-      (format 'nil "~{~a~^~&~}"
-              (mapcar #'generate-assign body-list))
-      (format 'nil "body;")))
-
-(defmethod generate ((generator verilog-generator) module stream)
-  "Generate Verilog for the provided MODULE, outputting the Verilog to STREAM."
-  (flet ((module-io->strings (direction args)
+(defmethod codegen ((verilog-module verilog-module) stream)
+  (flet ((module-io->strings (direction ios)
            "Generate Verilog argument string based on ARGS, noting the DIRECTION
 of the arguments in the string as well.
 
@@ -51,17 +57,14 @@ The two directions supported are the symbols 'input and 'output."
            ;; NOTE: We choose to only support the "wire" type in Verilog, for
            ;; module I/O, the same way Chisel did. Rather, Chisel does not
            ;; specify the type at all, which typically infers "wire".
-           (mapcar (lambda (arg)
+           (mapcar (lambda (io)
                      (format 'nil "~a ~a"
                              (string-downcase (symbol-name direction))
-                             (chil-sym->verilog-sym arg)))
-                   args)))
-
-    (format stream "module ~a " (chil-sym->verilog-sym (chil:module-name module)))
-    ;; TODO: Get indentation working
-    ;; Module parameters
+                             (chil-sym->verilog-sym (name io))))
+                   ios)))
+    (format stream "module ~a " (name verilog-module))
     ;; NOTE: Parameters only output when at least one parameter is provided.
-    (unless (uiop:emptyp (chil:module-parameters module))
+    (unless (uiop:emptyp (parameters verilog-module))
       (format stream "#(")
       (format stream "~%~{~a~^,~&~}~&"
               (mapcar (trivia:lambda-match
@@ -78,14 +81,108 @@ The two directions supported are the symbols 'input and 'output."
                         ;; incorrectly.
                         (_
                          (error "Modules must have 1 or 2 elements in their parameters.")))
-                      (chil:module-parameters module)))
+                      (parameters verilog-module)))
       (format stream ")~&"))
     ;; Module I/O
     (format stream "(")
+    ;; TODO: Get indentation working
+    ;; NOTE: We choose to only support the "wire" type in Verilog, for
+    ;; module I/O, the same way Chisel did. Rather, Chisel does not
+    ;; specify the type at all, which typically infers "wire".
     (format stream "~%~{~a~^,~&~}~&" ; ~% FORCES a line break!
-            (append (module-io->strings 'input (chil:module-inputs module))
-                    (module-io->strings 'output (chil:module-outputs module))))
+            (append (module-io->strings 'input (inputs verilog-module))
+                    (module-io->strings 'output (outputs verilog-module))))
     (format stream ");~&")
-    (format stream "~a~&" (generate-body (chil:module-body module)))
-    (format stream "endmodule // ~a" (chil-sym->verilog-sym
-                                      (chil:module-name module)))))
+    (codegen (body verilog-module) stream)
+    ;; (format stream "~a~&" )
+    (format stream "endmodule // ~a" (name verilog-module))))
+
+(defclass verilog-net (verilog-module)
+  ((name
+    :reader name
+    :initarg :name
+    :type string
+    :documentation "Name for this net."))
+  (:documentation "Verilog nets.
+
+A Verilog net is a combinational hardware circuit. They can be just a single
+wire or a very large combinational net."))
+
+(defmethod codegen ((net verilog-net) stream)
+  (format 'nil "~a" (name net)))
+
+;; TODO: This is how to define a type that can be used in :type, declare, and
+;; everywhere else that expects a "type" object.
+;; (deftype verilog-binops ()
+;;   '(member + - * / % bit-and bit-or))
+(defparameter *verilog-binops*
+  '(+ - * / % bit-and bit-or))
+(trivia:defun-ematch binops->verilog (op)
+  ('+ "+")
+  ('- "-")
+  ('* "*")
+  ('/ "/")
+  ('% "%")
+  ('bit-and "&")
+  ('bit-or "|"))
+
+;; FIXME: Should this subclass something else (like verilog-net)?
+;; Then anything that can drive a combinational net could be used here. However,
+;; a binary operation can be used in a sequential net too...
+;; There is something to think about here.
+;; The LHS & RHS of the binop should also be this abstract type too.
+(defclass verilog-binop (verilog-net)
+  ((op
+    :reader op
+    :initarg :op
+    ;; :type verilog-binops
+    :documentation "Operation to perform")
+   (lhs
+    :reader lhs
+    :initarg :lhs
+    :type verilog-net
+    :documentation "Left-Hand Side of Binary Operation")
+   (rhs
+    :reader rhs
+    :initarg :rhs
+    :type verilog-net
+    :documentation "Right-Hand Side of Binary Operation"))
+  (:documentation "Verilog's binary operations."))
+
+(defmethod codegen ((binop verilog-binop) stream)
+  (format 'nil "~a ~a ~a"
+          (codegen (lhs binop) stream)
+          (binops->verilog (op binop))
+          (codegen (rhs binop) stream)))
+
+(defclass verilog-assign (verilog-net)
+  ((target
+    :reader target
+    :initarg :target
+    :type verilog-net
+    :documentation "The target of a Verilog \"assign\" statement.")
+   (body
+    :reader body
+    :initarg :body
+    :type verilog-net
+    :documentation "The body to evaluate in a Verilog \"assign\" statement."))
+  (:documentation "Implementation of Verilog's \"assign\" statement."))
+
+(defmethod codegen ((component verilog-assign) stream)
+  ;; FIXME: verilog-assign codegen should NOT need to include a newline!
+  (format stream "assign ~a = ~a;~%"
+          (codegen (target component) stream)
+          (codegen (body component) stream)))
+
+
+;;;
+;;; Helpers for converting from Chil to Verilog
+;;;
+
+;; TODO: This should be done at the Chil -> Backend instantiation time.
+(defun chil-sym->verilog-sym (str)
+  "Replace characters in STR that are invalid for Verilog symbols."
+  ;; TODO: Need a better way to map characters to replace to known-good
+  ;; characters for that particular HDL.
+  (let ((chars-to-replace (cl-ppcre:create-scanner "-")))
+    (cl-ppcre:regex-replace-all chars-to-replace str "_")))
