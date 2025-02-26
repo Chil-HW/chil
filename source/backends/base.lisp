1805660addc5bb47301076d45b6104ef55147b90
Karl Hallsby <karl@hallsby.com>
2025-02-26 00:12:25 -0600

    Replace code generators with new abstract HDL base class & codegen generic

--- source/backends/base.lisp
+++ source/backends/base.lisp
@@ -16,18 +16,10 @@
 This is intended to be a way to retarget `Chil' modules to a particular
 target."))
 
-(defgeneric generate (backend-module stream)
-  (:documentation "Abstract method to generate the provided backend MODULE to
-STREAM"))
-
-;;; Abstract HDL generator class
-;; TODO: Make this a metaclass with MOP?
-(defclass hdl-generator ()
-  ()
-  (:documentation "Generic HDL back-end generator. Do NOT initialize this class!"))
-
 ;; NOTE: To generate a backend language to a file, use the following invocation:
 ;; (uiop:with-output-file (file "file.ext" :if-exists :supersede)
 ;;  (generate (make-instance 'chil/backends:generator) module file))
-(defgeneric generate (generator module stream)
-  (:documentation "Abstract method to generate the provided CHIL MODULE as another HDL."))
+(defgeneric codegen (backend-module stream)
+  (:documentation "Abstract method to perform code generation on the provided
+backend MODULE to STREAM. Every backend object that the user cares about should
+provide an implementation for `codegen'."))
