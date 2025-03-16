fd6e127d32bc0e456d1ac25b80b88e5390b5243d
Karl Hallsby <karl@hallsby.com>
2025-03-15 22:27:46 -0500

    Use combinatorial module defparameters in Verilator sim test

--- tests/sim.lisp
+++ tests/sim.lisp
@@ -26,44 +26,30 @@ run.")
                               ;; fails.
                               :keep 't
                               :type "v")
-     (let* ((inputs (list (make-instance 'verilog-net :name "x")
-                          (make-instance 'verilog-net :name "y")))
-            (outputs (list (make-instance 'verilog-net :name "f")))
-            (binop (make-instance 'verilog-binop :op '+
-                                                 :lhs (first inputs)
-                                                 :rhs (second inputs)))
-            (vmodule (make-instance 'verilog-module
-                                    :name (chil/backends:hyphen->underscore
-                                           "comb-binop")
-                                    :inputs inputs
-                                    :outputs outputs
-                                    :body (make-instance 'verilog-assign
-                                                         :target (first outputs)
-                                                         :body binop))))
-       ;; Turn off the DECLFILENAME lint flag, since we knowingly generate
-       ;; Verilog files that do NOT have the same name as the module they
-       ;; contain.
-       (format fstream "/* verilator lint_off DECLFILENAME */~%")
-       (chil/backends/verilog:codegen vmodule fstream)
-       (format fstream "/* verilator lint_off DECLFILENAME */~%")
-       (uiop:finish-outputs fstream)
+     ;; Turn off the DECLFILENAME lint flag, since we knowingly generate
+     ;; Verilog files that do NOT have the same name as the module they
+     ;; contain.
+     (format fstream "/* verilator lint_off DECLFILENAME */~%")
+     (chil/backends/verilog:codegen *combinatorial-binop-module* fstream)
+     (format fstream "/* verilator lint_off DECLFILENAME */~%")
+     (uiop:finish-outputs fstream)
 
-       ;; FIXME: Replace run-program with launch-program for async handling and
-       ;; parallel evaluation of tests. Need to use wait-process to get wait on
-       ;; the new process to finish. wait-process behaves like waitpid, returning
-       ;; the RC of the sub-process.
-       (multiple-value-bind (stdout stderr rc)
-           (uiop:run-program (list "verilator" "--lint-only"
-                                   "-Wall"
-                                   (uiop:native-namestring fpath))
-                             :output :string
-                             :error-output :string
-                             ;; Do not raise a continuable condition when
-                             ;; verilator has a non-zero exit code, since this
-                             ;; is a unit test and cannot do anything with them.
-                             :ignore-error-status 't)
-         ;; Only print program's stdout/stderr when test program's rc != 0.
-         (unless (zerop rc)
-           (format t "~a~%~%" stdout)
-           (format t "~a" stderr))
-         rc)))))
+     ;; FIXME: Replace run-program with launch-program for async handling and
+     ;; parallel evaluation of tests. Need to use wait-process to get wait on
+     ;; the new process to finish. wait-process behaves like waitpid, returning
+     ;; the RC of the sub-process.
+     (multiple-value-bind (stdout stderr rc)
+         (uiop:run-program (list "verilator" "--lint-only"
+                                 "-Wall"
+                                 (uiop:native-namestring fpath))
+                           :output :string
+                           :error-output :string
+                           ;; Do not raise a continuable condition when
+                           ;; verilator has a non-zero exit code, since this
+                           ;; is a unit test and cannot do anything with them.
+                           :ignore-error-status 't)
+       ;; Only print program's stdout/stderr when test program's rc != 0.
+       (unless (zerop rc)
+         (format t "~a~%~%" stdout)
+         (format t "~a" stderr))
+       rc))))
