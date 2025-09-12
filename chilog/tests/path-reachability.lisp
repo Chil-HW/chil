e8c87f1220e6e758b908662ea7336852f67aea8d
Karl Hallsby <karl@hallsby.com>
2025-09-11 23:37:04 -0500

    Add path reachability tests

--- chilog/tests/path-reachability.lisp
+++ chilog/tests/path-reachability.lisp
@@ -1,25 +1,59 @@
+(defpackage :chilog/tests/digraph
+  (:use :cl :lisp-unit2
+        :chilog :chilog/tests/utils))
+
+(in-package :chilog/tests/digraph)
+
 ;;; TODO: Implement these tests as sanity checks!
 ;;; They are small enough that we can hand-check that the results are sensible
 ;;; while not tanking performance if we have a bad Datalog interpreter and/or
 ;;; query planner.
 
 ;;; Digraph path reachability
-.decl edge(x : number, y : number)
-edge(1,2).
-edge(2,3).
-edge(3,4).
+;; .decl edge(x : number, y : number)
+;; edge(1,2).
+;; edge(2,3).
+;; edge(3,4).
 
-.decl edge(x : number, y : number)
-path(x,y) :- edge(x,y).
-path(x,z) :- edge(x,y), path(y,z).
-.output edge(IO=stdout)
+;; .decl path(x : number, y : number)
+;; path(x,y) :- edge(x,y).
+;; path(x,z) :- edge(x,y), path(y,z).
+;; .output path(IO=stdout)
 
-;;; Two alternative but equivalent definitions of digraph path reachability to
-;;; the recursive definition above.
-path(x,z) :- path(x,y), path(y,z).
-path(x,z) :- path(x,y), edge(y,z).
+;; ;;; Two alternative but equivalent definitions of digraph path reachability to
+;; ;;; the recursive definition above.
+;; path(x,z) :- path(x,y), path(y,z).
+;; path(x,z) :- path(x,y), edge(y,z).
 
-;;; Undirected edges (equivalence relations)
-.decl path(x : number, y : number) eqrel
-// or
-path(x,y) :- path(y,x).
+;; ;;; Undirected edges (equivalence relations)
+;; .decl path(x : number, y : number) eqrel
+;; // or
+;; path(x,y) :- path(y,x).
+
+(define-test digraph-reachability ()
+  (let ((db (make-instance 'chilog-db))
+        (X (make-instance 'chilog-variable :name "X"))
+        (Y (make-instance 'chilog-variable :name "Y"))
+        (Z (make-instance 'chilog-variable :name "Z"))
+        (edge (make-instance 'chilog-predicate :name "edge" :arity 2))
+        (path (make-instance 'chilog-predicate :name "path" :arity 2)))
+    (add-variable! X db)
+    (add-variable! Y db)
+    (add-variable! Z db)
+    (add-predicate! edge db)
+    (add-predicate! path db)
+    (add-fact! '(1 2) edge)
+    (add-fact! '(2 3) edge)
+    (add-fact! '(3 4) edge)
+    (add-rules! path (list X Y) (predicate->atom edge (list X Y)))
+    ;; path(X,Z) :- edge(X,Y), path(Y,Z).
+    (add-rules! path (list X Z)
+                (predicate->atom edge (list X Y))
+                (predicate->atom path (list Y Z)))
+    (chilog/interpreter:infer db)
+    (assert-set-equal
+     '((1 2) (2 3) (3 4)
+       (1 3) (2 4)
+       (1 4))
+     (facts (gethash "path" (predicates db)))
+     :test #'equal)))
